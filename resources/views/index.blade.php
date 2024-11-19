285bb84e9f48f93e8d7ae918d7f837a7338e72ba
Quentin Ruys <ruysquentin@gmail.com>
2024-11-19 10:02:42 +0100

    logo page home

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,11 @@
 @extends('layout')
 
 @section("content")
-    La super page d'Accueil
+<header>
+<h1> Picture </h1>
+    
+    <img src="logo.png" alt="Logo" width="500" height="500">
+        <h1>Albums Photos</h1>
+       
+    
 @endsection
