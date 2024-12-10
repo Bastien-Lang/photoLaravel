b9940b05783eb88ff9beeddd1659871f96088508
Bastien <bastien.lang11@gmail.com>
2024-12-10 10:44:41 +0100

    page d'accueil rework

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,21 +2,31 @@
 
 @section("content")
 <section class="albums">
-    @foreach($albums as $album)
-    <div class="album">
-        <a href="{{route('detailsAlbum', ['id' => $album->id])}}">
-            @if($album->lastPhoto)
-                <img src="{{$album->lastPhoto->url}}" alt="Dernière photo de l'album {{ $album->titre }}">
-            @else
-                <p>Aucune photo dans cet album.</p>
-            @endif
-            <h2>{{$album->titre}}</h2>
-            <p>{{$album->creation}}</p>
-            @if($album->lastPhoto)
-                <a href="{{route('detailsAlbum', ['id' => $album->id])}}" class="view-button">Voir l'album</a>
-            @endif
-        </a>
+    <div id="logo">
+        <img src="{{asset('logo-picture.png')}}" alt="logo">
     </div>
-    @endforeach
+    <div id="lasts">
+        <h1>Les 3 derniers albums</h1>
+    </div>
+    <div id="allAlbums">
+        @foreach($albums as $album)
+            <div class="album">
+                <a href="{{route('detailsAlbum', ['id' => $album->id])}}">
+                    @if($album->lastPhoto)
+                        <img src="{{$album->lastPhoto->url}}" alt="Dernière photo de l'album {{ $album->titre }}">
+                    @else
+                        <p>Aucune photo dans cet album.</p>
+                    @endif
+                    <h2>{{$album->titre}}</h2>
+                    <p>{{$album->creation}}</p>
+                    @if($album->lastPhoto)
+                        <a href="{{route('detailsAlbum', ['id' => $album->id])}}" class="view-button">Voir l'album</a>
+                    @endif
+                </a>
+            </div>
+        @endforeach
+    </div>
+    <h2>Une envie créative ?</h2>
+    <a href="{{route('ajouterAlbum')}}">Ajouter un album</a>
 </section>
 @endsection
